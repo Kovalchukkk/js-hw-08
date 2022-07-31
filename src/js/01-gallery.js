b0c28ffc2b985a65621df9be52daf65811fde771
Mykola Kovalchuk <kolyankov@gmail.com>
2022-07-31 21:05:50 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,4 +5,35 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = createMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', imagesMarkup);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}" target="_blank" rel="noopener noreferrer">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join('');
+}
