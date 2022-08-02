795a8528312ce0c87c9bbad6c4e3ec06b6b27dba
Kovalchukkk <71971559+Kovalchukkk@users.noreply.github.com>
2022-08-02 18:33:29 +0000

    Deploying to gh-pages from @ Kovalchukkk/js-hw-08@a44c66160cced50656e2ee4a312783db4eb4fa8f 🚀

--- 02-video.012e9607.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)})),o.on("timeupdate",onTime);
-//# sourceMappingURL=02-video.012e9607.js.map
