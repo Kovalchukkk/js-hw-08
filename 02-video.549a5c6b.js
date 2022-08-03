e7ce2e62613ced0b1bea2dc3a07d4ba1edf68c40
Kovalchukkk <71971559+Kovalchukkk@users.noreply.github.com>
2022-08-03 18:04:58 +0000

    Deploying to gh-pages from @ Kovalchukkk/js-hw-08@635ec3b4962c201ca71e72c105f9c4b24d6fa7fb 🚀

---
+++ 02-video.549a5c6b.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("play",(function(){console.log("played the video!")})),t.getVideoTitle().then((function(e){console.log("title:",e)}));localStorage.getItem("videoplayer-current-time");t.on("timeupdate",(function(e){var t=JSON.stringify(e);localStorage.setItem("videoplayer-current-time",t)}))}();
+//# sourceMappingURL=02-video.549a5c6b.js.map
--- 02-video.f79a17f4.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)})),o.on("timeupdate",(function(e){var o=JSON.stringify(e);localStorage.setItem("videoplayer-current-time",o)}))}();
-//# sourceMappingURL=02-video.f79a17f4.js.map
