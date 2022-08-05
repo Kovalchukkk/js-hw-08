e388445c3ae55fa36216ef0b1ca73f221149ea98
Mykola Kovalchuk <kolyankov@gmail.com>
2022-08-05 21:08:03 +0300

    add input handler

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,17 @@
 const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
 
 const refs = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('.feedback-form input'),
   textarea: document.querySelector('.feedback-form textarea'),
 };
+
+refs.form.addEventListener('input', onFormInput);
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  const formDataString = JSON.stringify(formData);
+
+  localStorage.setItem(STORAGE_KEY, formDataString);
+}
