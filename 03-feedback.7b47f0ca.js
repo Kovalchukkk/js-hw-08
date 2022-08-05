ec25a18042f64ca22a2aeb6d9a49d030ff7df373
Kovalchukkk <71971559+Kovalchukkk@users.noreply.github.com>
2022-08-05 17:31:31 +0000

    Deploying to gh-pages from @ Kovalchukkk/js-hw-08@ed5ffcb33a088c00893cb8aa3a08132082c263b9 🚀

---
+++ 03-feedback.7b47f0ca.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form"),document.querySelector(".feedback-form input"),document.querySelector(".feedback-form textarea");
+//# sourceMappingURL=03-feedback.7b47f0ca.js.map
