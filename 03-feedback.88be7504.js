1370ef02a936470628c7da2b6ee143dbc2455ea7
Kovalchukkk <71971559+Kovalchukkk@users.noreply.github.com>
2022-08-06 08:03:21 +0000

    Deploying to gh-pages from @ Kovalchukkk/js-hw-08@4a3bd6ef1e330f568b86a972e75ab6d12020778e 🚀

---
+++ 03-feedback.88be7504.js
@@ -0,0 +1,2 @@
+!function(){var e="feedback-form-state",t={};({form:document.querySelector(".feedback-form"),input:document.querySelector(".feedback-form input"),textarea:document.querySelector(".feedback-form textarea")}).form.addEventListener("input",(function(r){t[r.target.name]=r.target.value;var a=JSON.stringify(t);localStorage.setItem(e,a)}))}();
+//# sourceMappingURL=03-feedback.88be7504.js.map
